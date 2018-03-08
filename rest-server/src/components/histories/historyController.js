7bcf918661158e019186edc329ea30f73f838c60
downier <raulishere@gmail.com>
2018-03-07 16:40:40 -0800

    (dev) Working on async issues with forEach

--- rest-server/src/components/histories/historyController.js
+++ rest-server/src/components/histories/historyController.js
@@ -30,9 +30,8 @@ import { fetchUserQuery } from '../users/userQueries';
 export const fetchHistoryController = async (req, res) => {
   try {
     const { rows } = await historyQueryHelper(req.params);
-    let user;
     await rows.forEach(async (row) => {
-      user = await fetchUserQuery(row.receiver_id);
+      const user = await fetchUserQuery(row.receiver_id);
       row.receiver = user;
     });
     return res.status(200).send(rows);
