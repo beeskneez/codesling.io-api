f14ef87d067f3ea501070acd9a523ffc4ae747be
downier <raulishere@gmail.com>
2018-03-08 17:17:39 -0800

    (dev) Missing await within global query

--- rest-server/src/lib/components/globals.js
+++ rest-server/src/lib/components/globals.js
@@ -6,7 +6,7 @@ import {
 
 export const globalQueryHelper = async (payload, query, name) => {
   try {
-    const queryString = query(payload);
+    const queryString = await query(payload);
     const data = await db.queryAsync(queryString);
     success(`${name} - successfully retrived data ${JSON.stringify(data)}`);
     return data;
@@ -28,7 +28,7 @@ export const globalController = (query, name) => {
       payload = req.params;
     }
     try {
-      const { rows } = query(payload, url);
+      const { rows } = await query(payload, url);
       success(`${name} - sucessfully retrieved data ${JSON.stringify(rows)}`);
       return res.status(200).send(rows);
     } catch (err) {
