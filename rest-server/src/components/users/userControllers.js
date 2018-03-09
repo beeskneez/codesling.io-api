d934ef5e4ce11e6089c00930885ced566c0ba109
downier <raulishere@gmail.com>
2018-03-08 21:29:41 -0800

    (dev) New backend router to query DB by username

--- rest-server/src/components/users/userControllers.js
+++ rest-server/src/components/users/userControllers.js
@@ -1,7 +1,8 @@
 import db from '../../config/database';
 import {
-  fetchAllUserQuery
-} from './userQueries';
+  fetchAllUserQuery,
+  fetchUsernameQuery
+} from "./userQueries";
 import {
   success,
   error
@@ -14,6 +15,17 @@ export const fetchAllUserController = async (req, res) => {
     return res.status(200).send(data);
   } catch (err) {
     error('fetchAllUserController - error= ', error);
+    throw new Error(err);
+  }
+};
+
+export const fetchUsernameController = async (req, res) => {
+  try {
+    const data = await fetchUsernameQuery(req.params.username);
+    success("fetchUsernameController - successfully fetched data ", data);
+    return res.status(200).send(data);
+  } catch (err) {
+    error("fetchUsernameController - error= ", error);
     throw new Error(err);
   }
 };
